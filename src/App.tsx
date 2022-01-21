c8cb4296cf0a116ba57edcfdcc55ebdc2201fae5
Johann Rakotoharisoa <johann.rakotoharisoa@gmail.com>
2022-01-21 12:03:04 +0100

    💬 add draft slides

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Deck, FlexBox, Heading, Quote } from 'spectacle';
+import { Deck, FlexBox, Heading, Quote, Text } from 'spectacle';
 import { Template } from './theme/Template';
 import { theme } from './theme/config';
 import { Slide } from './theme/Slide';
@@ -20,6 +20,28 @@ const App = () => {
           <Heading>Introduction</Heading>
         </FlexBox>
       </Slide>
+      <Slide>
+        <Text>Hello, I'm Johann</Text>
+      </Slide>
+      <Slide>
+        <Text>What's Remix ?</Text>
+        <ul>
+          <li>A compiler</li>
+          <li>A server side HTTP handler</li>
+          <li>A server framework</li>
+          <li>A browser framework</li>
+        </ul>
+      </Slide>
+      <Slide>
+        <Text>Routes API</Text> - Folder Routes - Name Conventions
+      </Slide>
+      <Slide>
+        <Text>Route file</Text> - Loader - Action / Form - Other (meta, etc)
+      </Slide>
+      <Slide>
+        <Text>Progressive enhancement</Text>
+      </Slide>
+
       <Slide>
         <FlexBox justifyContent="center" alignItems="center" height="100%">
           <Quote>Get better at Remix, accidentally get better at the web</Quote>
