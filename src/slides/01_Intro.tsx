571b6d9a31b5643ca03f46b423a6604eaf167a49
Johann Rakotoharisoa <johann.rakotoharisoa@gmail.com>
2022-04-16 22:04:24 +0200

    ✨ add twitter

--- src/slides/01_Intro.tsx
+++ src/slides/01_Intro.tsx
@@ -1,10 +1,24 @@
 import { CenteredSlide } from '../theme/Slide';
 import remixLogo from '../images/remix-logo.svg';
+import { Text } from 'spectacle';
+import twitter from '../images/twitter.svg';
 
 export const Intro = () => (
   <CenteredSlide>
     <div>
-      <img width="800" src={remixLogo} alt="remix logo" />
+      <img width="500" src={remixLogo} alt="remix logo" />
+      <Text textAlign="center">
+        Devoxx France 2022 <br />
+        <img
+          src={twitter}
+          className="inline"
+          alt="logo"
+          style={{ height: '50px' }}
+        />{' '}
+        @ J o h a 2 n
+      </Text>
     </div>
   </CenteredSlide>
 );
+
+// <svg width="510" height="160" viewBox="220 180 730 260" fill="none" xmlns="http://www.w3.org/2000/svg">
--- src/slides/05_End.tsx
+++ src/slides/05_End.tsx
@@ -1,11 +1,16 @@
 import { CenteredSlide } from '../theme/Slide';
-import end from '../images/question-kent.gif';
 import { Appear, Heading } from 'spectacle';
+import twitterQrCode from '../images/twitter-qr-code.jpeg';
 export const End = () => (
   <CenteredSlide>
     <Heading>Merci !</Heading>
     <Appear>
-      <img src={end} alt="question" />
+      <img
+        src={twitterQrCode}
+        alt="profil"
+        className="aspect-square"
+        width={300}
+      />
     </Appear>
   </CenteredSlide>
 );
